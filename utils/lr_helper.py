15f5ee484510916795e3dffb7f35efaeafd4fb8e
lewandofskee <12232065@zju.edu.cn>
2023-12-17 22:30:45 +0800

    1

--- utils/lr_helper.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-
-def get_scheduler(optimizer, config):
-    if config.type == "StepLR":
-        return torch.optim.lr_scheduler.StepLR(optimizer, **config.kwargs)
-    else:
-        raise NotImplementedError
