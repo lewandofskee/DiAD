703329d390b5ab8cafa86af9acc32c62172db8b8
lewandofskee <12232065@zju.edu.cn>
2023-12-18 21:48:41 +0800

    update

--- ldm/models/autoencoder.py
+++ ldm/models/autoencoder.py
@@ -103,7 +103,7 @@ class AutoencoderKL(pl.LightningModule):
         x = batch[k]
         if len(x.shape) == 3:
             x = x[..., None]
-        x = x.permute(0, 3, 1, 2).to(memory_format=torch.contiguous_format).float()
+        # x = x.permute(0, 3, 1, 2).to(memory_format=torch.contiguous_format).float()
         return x
 
     def training_step(self, batch, batch_idx, optimizer_idx):
